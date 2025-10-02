83a1db55c3a07a846d33cc0a1490cfac4385e19a
David Sackstein <david.sackstein@fundguard.com>
2025-10-03 00:09:38 +0300

    Revert "remove EventFd"
    This reverts commit 30c2ab68a63a60efa8f628cd7845d957808d1ae4.

--- async_io/NotifySignal.h
+++ async_io/NotifySignal.h
@@ -1,22 +1,12 @@
 #pragma once
 
 #include <mutex>
-#include <unistd.h>
-#include <cerrno>
-#include <system_error>
+#include <cstdint>
+#include "EventFd.h"
 
 class NotifySignal {
 public:
-    NotifySignal() {
-        if (::pipe(_pipe) < 0) {
-            throw std::system_error(errno, std::generic_category(), "pipe() failed");
-        }
-    }
-
-    ~NotifySignal() {
-        ::close(_pipe[0]);
-        ::close(_pipe[1]);
-    }
+    NotifySignal() = default;
 
     // non-copyable
     NotifySignal(const NotifySignal&) = delete;
@@ -25,42 +15,20 @@ public:
     int arm() {
         std::lock_guard<std::mutex> lock(_mtx);
         if (_pending) {
-            read_one();
+            _efd.read();
             _pending = false;
         }
-        return _pipe[0];
+        return _efd.get();
     }
 
     void notify() {
         std::lock_guard<std::mutex> lock(_mtx);
-        write_one();
+        _efd.write();
         _pending = true;
     }
 
 private:
-    void write_one() {
-        char b = 1;
-        ssize_t result;
-        while ((result = ::write(_pipe[1], &b, 1)) < 0 && errno == EINTR) {
-            // retry on EINTR
-        }
-        if (result < 0) {
-            throw std::system_error(errno, std::generic_category(), "notify write failed");
-        }
-    }
-
-    void read_one() {
-        char b;
-        ssize_t result;
-        while ((result = ::read(_pipe[0], &b, 1)) < 0 && errno == EINTR) {
-            // retry on EINTR
-        }
-        if (result < 0) {
-            throw std::system_error(errno, std::generic_category(), "notify drain failed");
-        }
-    }
-
-    int _pipe[2];
+    EventFd _efd;
     std::mutex _mtx;
     bool _pending = false;
 };
